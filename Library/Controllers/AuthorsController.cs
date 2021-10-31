67e3977ba8a950614ce2b21e32388cb26a1322f7
Alex Bertotto <ajb5206@gmail.com>
2021-10-31 12:31:30 -0700

    add create, delete, details, and edit routes and views for authors
    Co-authored-by: Amerik Velasquez <amerik.velasquez22@gmail.com>

--- Library/Controllers/AuthorsController.cs
+++ Library/Controllers/AuthorsController.cs
@@ -20,5 +20,55 @@ namespace Library.Controllers
       List<Author> model = _db.Authors.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Author Author)
+    {
+      _db.Authors.Add(Author);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisAuthor = _db.Authors
+        .Include(author => author.JoinEntities)
+        .ThenInclude (join => join.Book)
+        .FirstOrDefault(author => author.AuthorId == id);
+      return View(thisAuthor);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisAuthor = _db.Authors.FirstOrDefault( author => author.AuthorId == id);
+      return View(thisAuthor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Author author)
+    {
+      _db.Entry(author).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisAuthor = _db.Authors.FirstOrDefault(author => author.AuthorId == id);
+      return View(thisAuthor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisAuthor = _db.Authors.FirstOrDefault(author => author.AuthorId == id);
+      _db.Authors.Remove(thisAuthor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
