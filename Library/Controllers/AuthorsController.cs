aea96855662e36385b5e2571939a9c31ffb0fba3
Alex Bertotto <ajb5206@gmail.com>
2021-10-31 11:48:28 -0700

    add initial model, view and controller set up for author-book relationship
    Co-authored-by: Amerik Velasquez <amerik.velasquez22@gmail.com>

---
+++ Library/Controllers/AuthorsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Library.Controllers 
+{
+  public class AuthorsController : Controller 
+  {
+    private readonly LibraryContext _db;
+    
+    public AuthorsController(LibraryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Author> model = _db.Authors.ToList();
+      return View(model);
+    }
+  }
+}
